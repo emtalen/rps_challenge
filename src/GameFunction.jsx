c8fb29d02122435eb5c8d5c135c46ef58371a26c
Emma_Thalen <thlalen.emma@gmail.com>
2020-02-26 08:32:01 +0100

    starting with the game function

--- src/GameFunction.jsx
+++ src/GameFunction.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
-const GameFunction = () => {
-    
-    return
+const GameFunction = ({playerMove}) => {
+  const computerMove = getComputerMove()
+  return (
+    <div id='resultelement'>
+      <p>You chose {playerMove} and the computer chose {computerMove}</p>
+      </div>
+  )
 }
 
 export default GameFunction
