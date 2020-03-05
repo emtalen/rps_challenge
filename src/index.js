408925d31da9f772c77c111e3fc7651d3dcb9a4f
emtalen <thlalen.emma@gmail.com>
2020-03-05 23:10:49 +0100

    changes in the routes for the best of three

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import BestOfThree from './BestOfThree';
 import SimpleGame from './SimpleGame';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Welcome from './App';
@@ -13,7 +12,7 @@ const App = () => {
             <Switch>
                 <Route exact path='/' component={Welcome}></Route>
                 <Route exact path='/simplegame' component={SimpleGame}></Route>
-                <Route exact path='/bestofthree' component={BestOfThree}></Route>
+                {/* <Route exact path='/bestofthree' component={BestOfThree}></Route> */}
             </Switch>
         </div>
     )
