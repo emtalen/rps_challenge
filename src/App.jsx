da20303f9a28a0d721254ab20d990d12a9fa55a5
emtalen <thlalen.emma@gmail.com>
2020-03-06 09:19:59 +0100

    commenting out the button for best of three

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const Welcome = () => {
         <div class="selectgame">
           <h2 id='subtitle'>How do you want to play?</h2>
           <button class="massive ui button" id='simplegame-tab' onClick={event =>  window.location.href='/simplegame'}>Scores per win</button>
-          <button class="massive ui button" id='bestofthree-tab' onClick={event =>  window.location.href='/bestofthree'}>Best of three</button>
+          {/* <button class="massive ui button" id='bestofthree-tab' onClick={event =>  window.location.href='/bestofthree'}>Best of three</button> */}
         </div>
     </div>
   )
